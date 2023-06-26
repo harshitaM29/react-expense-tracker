b777734d8d51cd40be07e0c4677acc7adf2cf0e2
harshitaM29 <harshita.mundhe29@gmail.com>
2023-06-26 18:34:54 +0530

    added a get request for getting saved data in firebase

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,40 @@
-import { Fragment, useContext, useRef } from 'react';
+import { Fragment, useCallback, useContext, useEffect, useRef, useState } from 'react';
 import classes from './Profile.module.css';
 import AuthContext from '../../store/auth-context';
 
 const Profile = () => {
 
+    const [percent,setPercent] = useState(64)
     const authCtx = useContext(AuthContext);
     const nameInputRef = useRef();
     const photoURLRef = useRef();
+    
+
+    const getData = useCallback(() => {
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBJVhLyraZIaOHKuIYAEHRRqOQ1pCR7qsw', {
+        method: 'POST',
+        body:JSON.stringify({
+          idToken:authCtx.tokenId
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => {
+        if(res.ok){
+          res.json().then(data => {
+            console.log(data.users.email);
+            setPercent(100)
+            nameInputRef.current.value = data.users.map(item => item.displayName)
+            photoURLRef.current.value = data.users.map(item => item.photoUrl)
+          })
+        }
+      })
+    })
     const submitHandler = (e) => {
         e.preventDefault();
         const enteredName = nameInputRef.current.value;
         const enteredPhotoURL = photoURLRef.current.value;
+       
         fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBJVhLyraZIaOHKuIYAEHRRqOQ1pCR7qsw', {
             method:'POST',
             body:JSON.stringify({
@@ -20,14 +44,31 @@ const Profile = () => {
                 returnSecureToken:false,
                 
             })
+        }).then(res => {
+          if(res.ok) {
+            res.json().then(data => {
+              setPercent(100)
+             console.log(data)
+            });
+          } else {
+            res.json().then(data => {
+              alert(data.error.message)
+              })
+          }
         })
 
     }
+
+    useEffect(() => {
+      getData();
+    }, [getData])
     return (
         <Fragment>
         <div className={classes.profile}>
         <h3>Complete Your Profile</h3>
-        
+        <div>
+          <p>Your Profile is {percent}% completed</p>
+        </div>
         </div>
 
         <section className={classes.form}>
@@ -35,13 +76,14 @@ const Profile = () => {
               <form onSubmit={submitHandler}>
                 <div className={classes.control}>
                   <label htmlFor='name'>Full Name</label>
-                  <input type='text' id='name' required />
+                  <input type='text' id='name' ref={nameInputRef} required />
                 </div>
                 <div className={classes.control}>
                   <label htmlFor='photo'>Photo URL</label>
                   <input
-                    type='text'
+                    type='url'
                     id='photo'
+                    ref={photoURLRef}
                    
                     required
                   />
